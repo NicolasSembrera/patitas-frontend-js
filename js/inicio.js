e8f62d63c9685c50d71d54a0e1403e7b63b4c4e6
NicolasSembrera <nsembrera@gmail.com>
2024-10-13 13:27:56 -0500

    Proyecto Patitas JS - T2

---
+++ js/inicio.js
@@ -0,0 +1,92 @@
+/**
+ * Se ejecuta cuando la pagina ha cargado completamente (DOM, CSS, JS, Images, etc.)
+ * En caso desees ejecutar el JS a penas se haya cargado el DOM, puedes usar:
+ * -> document.addEventListener('DOMContentLoaded', {});
+ * -> De lo contrario en la importación del script utilizar el atributo "defer"
+ */
+window.addEventListener("load", function () {
+  // recuperar controles del formulario
+  const tipoDocumento = document.getElementById("tipoDocumento");
+  const numeroDocumento = document.getElementById("numeroDocumento");
+  const password = document.getElementById("password");
+  const btnIngresar = document.getElementById("btnIngresar");
+  const msgError = document.getElementById("msgError");
+
+  if (window.localStorage.getItem("cerrar") !== null) {
+    this.alert(window.localStorage.getItem("cerrar"));
+    window.localStorage.removeItem("cerrar")
+  }
+
+  // implementar listener
+  btnIngresar.addEventListener("click", function () {
+    // validar campos de entrada
+    if (
+      tipoDocumento.value === null ||
+      tipoDocumento.value.trim() === "" ||
+      numeroDocumento.value === null ||
+      numeroDocumento.value.trim() === "" ||
+      password.value === null ||
+      password.value.trim() === ""
+    ) {
+      mostrarAlerta("Error: Debe completar correctamente sus credenciales");
+      return;
+    }
+    ocultarAlerta();
+    autenticar();
+  });
+});
+
+function mostrarAlerta(mensaje) {
+  msgError.innerHTML = mensaje;
+  msgError.style.display = "block";
+}
+
+function ocultarAlerta() {
+  msgError.innerHTML = "";
+  msgError.style.display = "none";
+}
+
+async function autenticar() {
+  const url = "http://localhost:8085/login/autenticar-async";
+  const data = {
+    tipoDocumento: tipoDocumento.value,
+    numeroDocumento: numeroDocumento.value,
+    password: password.value,
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      mostrarAlerta("Error: Ocurrió un problema en la autenticación1");
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    // validar respuesta
+    const result = await response.json();
+    console.log("Respuesta del servidor:", result);
+
+    if (result.codigo === "00") {
+      localStorage.setItem("result", JSON.stringify(result));
+
+      const datos = {
+        tipoDocumento: tipoDocumento.value,
+        numeroDocumento: numeroDocumento.value,
+      };
+
+      localStorage.setItem("otrosDatos", JSON.stringify(datos));
+      window.location.replace("principal.html");
+    } else {
+      mostrarAlerta(result.mensaje);
+    }
+  } catch (error) {
+    console.error("Error: Ocurrió un problema en la autenticación. ", error);
+    mostrarAlerta("Error: Ocurrió un problema en la autenticación");
+  }
+}
---
+++ js/principal.js
@@ -0,0 +1,78 @@
+window.addEventListener("load", function () {
+  const msgSuccess = document.getElementById("msgSuccess");
+  const btnCerrarSesion = this.document.getElementById("btnCerrarSesion");
+
+  // recuperar nombre de usuario
+  const result = JSON.parse(window.localStorage.getItem("result"));
+  mostrarAlerta(result.nombreUsuario);
+
+   
+  //CierreSesion
+  btnCerrarSesion.addEventListener("click", function () {
+
+    var data = {
+        tipoDocumento: "",
+        numeroDocumento: "",
+    };
+    
+    data = JSON.parse(window.localStorage.getItem("otrosDatos"));
+    const numeroDocumento = data.numeroDocumento;
+    const tipoDocumento = data.tipoDocumento;
+
+    logout(tipoDocumento, numeroDocumento);
+  });
+});
+
+function mostrarAlerta(mensaje) {
+  msgSuccess.innerHTML = mensaje;
+  msgSuccess.style.display = "block";
+}
+
+function ocultarAlerta() {
+  msgSuccess.innerHTML = "";
+  msgSuccess.style.display = "none";
+}
+
+function obtenerFechaActual() {
+  const fecha = new Date();
+
+  const dia = String(fecha.getDate()).padStart(2, "0");
+  const mes = String(fecha.getMonth() + 1).padStart(2, "0"); // Los meses comienzan en 0
+  const anio = fecha.getFullYear();
+
+  const horas = String(fecha.getHours()).padStart(2, "0");
+  const minutos = String(fecha.getMinutes()).padStart(2, "0");
+
+  return `${dia}/${mes}/${anio} ${horas}:${minutos}`;
+}
+
+async function logout(tipoDocumento, numeroDocumento) {
+  const url = "http://localhost:8085/login/log_out";
+  const data = {
+    tipoDocumento: tipoDocumento,
+    numeroDocumento: numeroDocumento,
+    FechaCierre: obtenerFechaActual(),
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      mostrarAlerta("Error: Ocurrió un problema en la autenticación1");
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    localStorage.setItem("cerrar", "Cierre de Sesión Exitoso");
+    window.location.replace("indice.html");
+
+  } catch (error) {
+    console.error('Error: Ocurrió un problema al cerrar Sesión. ', error);
+    mostrarAlerta('Error: Ocurrió un problema al cerrar Sesión.');
+  }
+}
